9ddaa023a5ed0e499ce08679975d9290e3553369
Mamoun Benmeradi <mamoun@benmeradi.com>
2016-11-15 16:18:26 +0100

    Finished exercice

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Task.create(title: "Trello", description: "Mettre à jour son trello", status: "0")
+Task.create(title: "Payer LeWagon", description: "Faire un virement à Romain", status: "0")
+Task.create(title: "Booba", description: "Ecouter le dernier album de booba", status: "0")
+Task.create(title: "Voiture", description: "Emmener la voiture chez le garagiste", status: "0")
